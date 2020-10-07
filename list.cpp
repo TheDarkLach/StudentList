ab49a3db139b9062d76eb8fff88495c23be6abe6
TheDarkLach <11thunderstorm11@gmail.com>
2020-10-07 12:08:03 -0700

    idk

--- list.cpp
+++ list.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
+struct student
 {
-  //yeaaaa baby
+  char fName [25];
+  char lName[25];
+  int id;
+  float gpa;
+};
+
+void add()
+{
+  student s;
+
+  cout << "Enter first name" << endl;
+  cin >> s.fName;
+  
+  cout << "Enter last name" << endl;
+  cin >> s.lName;
+
+  cout << "Enter student id" << endl;
+  cin >> s.id;
+
+  cout << "Enter gpa" << endl;
+  cin >> s.gpa;
+}
+
+void print()
+{
+
+}
+
+void del()
+{
+
+}
+
+int main() 
+{
+  cout << "Would you like to ADD, DELETE, or PRINT" << endl;
+
+  //cout << "Name: " << s.fName << endl;
+  //cout << "Roll: " << s.id << endl;
+  //cout << "Marks: " << s.gpa << endl;
 }
