9e4c65b0eab467e1397f95f061e133e7c5e0cef4
TheDarkLach <11thunderstorm11@gmail.com>
2020-10-07 12:36:42 -0700

    yes

--- list.cpp
+++ list.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
 struct student
@@ -13,17 +15,19 @@ void add()
 {
   student s;
 
-  cout << "Enter first name" << endl;
+  cout << "\nEnter first name" << endl;
   cin >> s.fName;
   
-  cout << "Enter last name" << endl;
+  cout << "\nEnter last name" << endl;
   cin >> s.lName;
 
-  cout << "Enter student id" << endl;
+  cout << "\nEnter student id" << endl;
   cin >> s.id;
 
-  cout << "Enter gpa" << endl;
+  cout << "\nEnter gpa" << endl;
   cin >> s.gpa;
+
+  cout << "Student entered!" << endl;
 }
 
 void print()
@@ -38,7 +42,33 @@ void del()
 
 int main() 
 {
+  char cmd[10];
+  char ADD[] = "ADD";
+  char DELETE[] = "DELETE";
+  char PRINT[] = "PRINT";
+
   cout << "Would you like to ADD, DELETE, or PRINT" << endl;
+  cin >> cmd;
+
+  if (strcmp(cmd,ADD) == 0)
+  {
+    cout << "add" << endl;
+    add();
+  }
+  else if (strcmp(cmd,DELETE) == 0)
+  {
+    cout << "delete" << endl;
+    del();
+  }
+  else if (strcmp(cmd,PRINT) == 0)
+  {
+    cout << "print" << endl;
+    print();
+  }
+  else
+  {
+    cout << "yeaa baby";
+  }
 
   //cout << "Name: " << s.fName << endl;
   //cout << "Roll: " << s.id << endl;
