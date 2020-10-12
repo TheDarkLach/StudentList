60cde0624ffbd09dfc5a2ec452de1b65ef68a044
TheDarkLach <11thunderstorm11@gmail.com>
2020-10-11 19:08:24 -0700

    done

--- list.cpp
+++ list.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cstring>
 #include<vector>
+#include <iomanip>
 
 using namespace std;
 
+//struct for the students
 struct student
 {
   char* fName;
@@ -18,7 +20,7 @@ ostream &operator<<(ostream &os, const student &val)
   os
     << "Name: " << val.fName << " " << val.lName
     << ", ID: " << val.id
-    << ", GPA: " << val.gpa
+    << ", GPA: " << setprecision(2) << fixed << val.gpa
     << endl;
 
   return os;
@@ -27,63 +29,7 @@ ostream &operator<<(ostream &os, const student &val)
 void add(vector<student*>*);
 //void add(vector<student*>& paramlist);
 
-int main() 
-{
-  bool run = true;
-  vector <student*>* list = new vector<student*>();
-
-  char cmd[10];
-
-  while (run == true)
-  {
-    cout << "\nWould you like to ADD, DELETE, PRINT, or QUIT" << endl;
-    cin >> cmd;
-
-    if (strcmp(cmd,"ADD") == 0)
-    {
-      add(list);
-      cout << "Student entered!" << endl;
-      
-    }
-    else if (strcmp(cmd,"DELETE") == 0)
-    {
-      int ID;
-      cout << "ID of student you want to delete: ";
-
-      cin >> ID;
-      int position = 0;
-      for (vector<student*>::iterator j = list->begin(); j != list->end(); ++j)
-      {
-        if((*j) -> id == ID)
-        {
-          list->erase(list->begin() + position);
-          break;
-        }
-        else
-        {
-          position++;
-        }
-      }
-
-    }
-    else if (strcmp(cmd,"PRINT") == 0)
-    {
-      for(vector<student*>::iterator i = list->begin(); i != list->end(); i++)
-      {
-        cout << **i;
-      }
-    }
-    else if (strcmp(cmd,"QUIT") == 0)
-    {
-      exit(0);
-    }
-    else
-    {
-      cout << "yeaa baby";
-    }
-
-  }
-}
+//adding
 void add(vector<student*>* paramlist)
 {
   student* s = new student();
@@ -96,11 +42,88 @@ void add(vector<student*>* paramlist)
   cout << "Enter last name" << endl;
   cin >> s->lName;
 
-  cout << "Enter id number" << endl;
+  cout << "Enter ID" << endl;
   cin >> s->id;
 
   cout << "Enter GPA" << endl;
   cin >> s->gpa;
 
   paramlist->push_back(s);
+}
+
+void del(vector<student*> *list)
+{
+  int ID;
+  cout << "ID of student you want to delete: ";
+
+  cin >> ID;
+  int position = 0;
+  //iterate through vector
+  for (vector<student*>::iterator j = list->begin(); j != list->end(); ++j)
+  {
+    //if given value is equal to a value in vector
+    if((*j) -> id == ID)
+    {
+      list->erase(list->begin() + position);
+      break;
+    }
+    else
+    {
+      position++;
+    }
+  }
+}
+
+void print(vector<student*> *list)
+{
+  //iterate through
+  for(vector<student*>::iterator i = list->begin(); i != list->end(); i++)
+  {
+    cout << **i;
+  }
+}
+
+int main() 
+{
+  bool run = true;
+  //vector
+  vector <student*>* list = new vector<student*>();
+
+  char cmd[10];
+
+  while (run == true)
+  {
+    cout << "\nWould you like to ADD, DELETE, PRINT, or QUIT" << endl;
+    cin >> cmd;
+
+    //add student
+    if (strcmp(cmd,"ADD") == 0)
+    {
+      add(list);
+      cout << "Student entered!" << endl;
+      
+    }
+    //delete student
+    else if (strcmp(cmd,"DELETE") == 0)
+    {
+      del(list);
+      cout << "Student deleted!";
+
+    }
+    //print out students
+    else if (strcmp(cmd,"PRINT") == 0)
+    {
+      print(list);
+    }
+    //quitting
+    else if (strcmp(cmd,"QUIT") == 0)
+    {
+      exit(0);
+    }
+    else
+    {
+      cout << "yeaa baby";
+    }
+
+  }
 }
